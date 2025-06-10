32d189e1426bef21d678e18597d2ca5eb3a54124
oiclai <clarabpnalcantara@gmail.com>
2025-06-10 01:23:41 -0300

    :sparkles: feat: implement basic player vs player move logic

--- JogoDaVelha.java
+++ JogoDaVelha.java
@@ -21,22 +21,44 @@ public class JogoDaVelha {
     private boolean contraMaquina;
     private int nivelEsperteza; // 1 ou 2
 
-    public JogoDaVelha(String simbolo1, String simbolo2) { // jxj
+    public JogoDaVelha(String simbolo1, String simbolo2) { // construtor jxj
         this.simbolos[0] = simbolo1; this.simbolos[1] = simbolo2;
         this.contraMaquina = false;
         // iniciar tabuleiro p poder ficar acrecentando os simbolos nas posições
         for (int i=0; i<9; i++){
-
+            this.celulas[i] = "";// ai if ( celulas[posicao].equals("")) == true = livre
         }
     }
+    public JogoDaVelha(String nomeJogador1, int nivel) { } // consturo mxj !!!!!!!!!!!!!!!!!!
 
-    public JogoDaVelha(String nomeJogador1, int nivel) {}
+    public void jogaJogador(int numeroJogador, int posicao) {
+     // numerojogador é o indice da lista la do simbolo p acessar x ou bola
+         if (numeroJogador < 0||numeroJogador > 1) return; // casos estranhos
+         if (posicao <0||posicao >= 9) return; //fora do limtie
+         if (!celulas[posicao].equals("")) return; // já está ocupada (basica)
 
-    public void jogaJogador(numeroJogador, posicao) {}
+         String simbolo = simbolos[numeroJogador]; // x ou bola
+         celulas[posicao] = simbolo; // nova atribuição ao elem.da poscuao
+         historico.put(posicao, simbolo);
+         quantidadeJogadas++;
+        }
+    public void jogaMaquina() {} // maquina !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
-    public boolean terminou() {}
+    public boolean terminou() {
+        return (getResultado() != -1)||(quantidadeJogadas == 9) ; //ganhou ou empate
+    }
 
-    public int getResultado() {}
+    public int getResultado() {
+        // pra nao usar matriz....
+        int[] vitoriaLinha1 = {0, 1, 2};
+        int[] vitoriaLinha2 = {3, 4, 5};
+        int[] vitoriaLinha3 = {6, 7, 8};
+        int[] vitoriaColuna1 = {0, 3, 6};
+        int[] vitoriaColuna2 = {1, 4, 7};
+        int[] vitoriaColuna3 = {2, 5, 8};
+        int[] vitoriaDiagonal1 = {0, 4, 8};
+        int[] vitoriaDiagonal2 = {2, 4, 6};
+    }
 
     public String getSimbolo(int numeroJogador) {}
 
